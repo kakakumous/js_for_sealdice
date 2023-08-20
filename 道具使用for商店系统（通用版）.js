ef2f1566fb81ba1b26e9cea7944eef410b7facd5
kakakumous <129272724+kakakumous@users.noreply.github.com>
2023-08-20 20:20:07 +0800

    展示道具分页&对他人使用

--- 道具使用for商店系统（通用版）.js
+++ 道具使用for商店系统（通用版）.js
@@ -1,28 +1,32 @@
 // ==UserScript==
 // @name         道具使用for商店系统
 // @author       kakakumous
-// @version      1.0.0
-// @description  自用的道具使用dlc 高度参考并引用步棋商店系统插件（扩展的扩展.jpg,数据库共享） 详情请到github链接或直接查看脚本
+// @version      1.0.1
+// @description  【尚未进行全面测试，发现bug欢迎git留言】自用的道具使用dlc 高度参考并引用步棋商店系统插件（扩展的扩展.jpg,数据库共享） 骰主使用教程请到github链接查看readme
 // @timestamp    1692412501
 // 2023-08-19 10:35:01
 // @license      MIT
 // @homepageURL  https://github.com/kakakumous/js_for_sealdice
 // ==/UserScript==
 
-//TODO:分页查看全部的道具信息 也没那么多吧（目移）
-//TODO:对at的人使用道具
-
 let ext = seal.ext.find("shop");
 if (!ext) {
     ext = seal.ext.new("shop", "檀轶步棋", "1.0.0");
     seal.ext.register(ext);
 }
+//在此定义骰主.展示道具时每页展示的道具数
+const ITEMS_PER_PAGE=7;
+//在此定义限时buff的最大堆叠层数
+const INIT_MAX_STATUS=7;
+
 const UseErrno = {
     1: "背包中无此物品",
     2: "背包中此物品数量不足",
     3: "此物品尚未实装omo",
     4: "啊哦 实装的effect出了点问题 请敲打策划",
-    5: "冥冥中一种神奇的直觉让你放弃了使用（有更强力的效果无法覆盖）"
+    5: "冥冥中一种神奇的直觉让你放弃了使用（有更强力的效果无法覆盖）",
+    6: "这个物品不能被使用",
+    7: "这个物品使用需定义影响变量的最大值"
 };
 class ItemInfo {
     itemInfo;
@@ -65,14 +69,18 @@ class ItemInfo {
             }
         }
     }
-    present() {
+    present(page) {
         if (this.itemInfo.length <= 0) {
             return "还没有任何道具信息";
         }
+        if(this.itemInfo.length<(page-1)*ITEMS_PER_PAGE-1){
+            return "这一页没有道具信息";
+        }
         let arr = [];
-        for (let i of this.itemInfo) {
-            let str=`~ ${i.name}-影响变量: ${i.effectType}${i.effect}`;
-            if(i.ifStatus==1)str=str+`持续时间+${i.upTime}`;
+        for (let i=(page-1)*ITEMS_PER_PAGE;i<page*ITEMS_PER_PAGE;i++) {
+            console.log(this.itemInfo[i].name);
+            let str=`~ ${this.itemInfo[i].name}-影响变量: ${this.itemInfo[i].effectType}${this.itemInfo[i].effect}`;
+            if(this.itemInfo[i].ifStatus==1)str=str+`持续${this.itemInfo[i].upTime}小时`;
             arr.push(str);
         }
         return arr.join("\n");
@@ -165,26 +173,35 @@ class Rucksack {
         }else if(!itemOverview){
             return 3;
         }
+        if(itemOverview.effectType===`#`){
+            return 6;
+        }
+
+        let effectType=itemOverview.effectType;
+        let effect=itemOverview.effect;
+        //初始化没有的变量=0
+        if(seal.vars.intGet(mctx, effectType)[1]==false){
+            seal.vars.intSet(mctx, effectType,0);
+        }
+        let old_effect=seal.vars.intGet(mctx, effectType)[0];
+        let new_effect=old_effect;
+
         if(itemOverview.ifStatus==0){//非状态道具 多用叠数值
-            let effectType=itemOverview.effectType;
-            let effect=itemOverview.effect;
-            //初始化没有的变量=0
-            if(seal.vars.intGet(mctx, effectType)[1]==false){
-                seal.vars.intSet(mctx, effectType,0);
-            }
             //初始化没有的最大值=-1（无上限）
             if(seal.vars.intGet(mctx, effectType+`_MAX`)[1]==false){
                 seal.vars.intSet(mctx, effectType+`_MAX`,-1);
-                console.log(`初始化成功`);
             }
-            let old_effect=seal.vars.intGet(mctx, effectType)[0];
-            let new_effect=old_effect;
+            let effect_MAX=seal.vars.intGet(mctx, effectType+`_MAX`)[0];
 
             const opType=effect.charAt(0);
-            console.log(opType);
-            const num=parseFloat(effect.slice(1,effect.length));
+            let num = 0;
+            if(effect.endsWith(`%`)){
+                if(effect_MAX==-1){return 7;}
+                num=0.01*effect_MAX*parseFloat(effect.slice(1,effect.length-1));
+            }else{
+                num=parseFloat(effect.slice(1,effect.length));
+            }
             console.log(num);
-            
             switch(opType){
                 case `=`:new_effect=num;break;
                 case `+`:new_effect=old_effect+num*quantity;break;
@@ -192,32 +209,33 @@ class Rucksack {
                 case `*`:new_effect=old_effect*Math.pow(num,quantity);break;
                 default:return 4;
             }
-            if(seal.vars.intGet(mctx, effectType+`_MAX`)[0]!=-1&&seal.vars.intGet(mctx, effectType+`_MAX`)[0]<new_effect){
-                seal.vars.intSet(mctx, effectType,seal.vars.intGet(mctx, effectType+`_MAX`)[0]);
+            if(effect_MAX!=-1&&effect_MAX<new_effect){
+                seal.vars.intSet(mctx, effectType,effect_MAX);
             }else{
                 seal.vars.intSet(mctx, effectType,new_effect);
             }
         }else{//状态道具 多用叠时间
             const timestamp = Date.parse(new Date());//秒级时间戳
-            let effectType=itemOverview.effectType;
-            let effect=itemOverview.effect;
+
             let upHours=itemOverview.upTime*quantity;
-            console.log(`试图初始化没有的变量=0`);
-            if(seal.vars.intGet(mctx, effectType)[1]==false){
-                seal.vars.intSet(mctx, effectType,0);
-            }            
-            console.log(`试图初始化没有的持续时间=0`);
+            //初始化没有的最大值=7(限时效果最多堆叠7层)
+            if(seal.vars.intGet(mctx, effectType+`_MAX`)[1]==false){
+                seal.vars.intSet(mctx, effectType+`_MAX`,INIT_MAX_STATUS);
+            }        
             if(seal.vars.intGet(mctx, effectType+`_upTime`)[1]==false){
                 seal.vars.intSet(mctx, effectType+`_upTime`,timestamp);
             }
 
-            let old_effect=seal.vars.intGet(mctx, effectType)[0];
-            let new_effect=old_effect;
-
+            let effect_MAX=seal.vars.intGet(mctx, effectType+`_MAX`)[0];
             const opType=effect.charAt(0);
-            console.log(opType);
-            const num=parseInt(effect.slice(1,effect.length));
+            let num = 0;
+            if(effect.endsWith(`%`)){
+                num=0.01*effect_MAX*parseFloat(effect.slice(1,effect.length-1));
+            }else{
+                num=parseFloat(effect.slice(1,effect.length));
+            }
             console.log(num);
+
             switch(opType){
                 case `=`:new_effect=num;break;
                 case `+`:new_effect=old_effect+num;break;
@@ -228,7 +246,11 @@ class Rucksack {
             if(old_effect>new_effect&&seal.vars.intGet(mctx, effectType+`_upTime`)[0]>timestamp){
                 return 5;//有未到时间限制的更强力效果 无法覆盖
             }
-            seal.vars.intSet(mctx, effectType,new_effect);
+            if(effect_MAX!=-1&&effect_MAX<new_effect){
+                seal.vars.intSet(mctx, effectType,effect_MAX);
+            }else{
+                seal.vars.intSet(mctx, effectType,new_effect);
+            }
             console.log(old_effect+`->`+new_effect);
             console.log(effectType+`=`+seal.vars.intGet(mctx, effectType)[0]);
             seal.vars.intSet(mctx, effectType+`_upTime`,timestamp+upHours*3600);
@@ -237,10 +259,10 @@ class Rucksack {
         return 0;
     }
 }
-//=============================================================骰主对道具信息的操作
+//=======================================================================================骰主对道具信息的操作
 let cmdAddItemInfo = seal.ext.newCmdItemInfo();
 cmdAddItemInfo.name = "AddItemInfo";
-cmdAddItemInfo.help = ".添加道具信息 <名称> <影响变量（例：$m金币）> <影响变量参数（=+-*操作符开头，例：+1即为金币+1）> <描述> <使用回执> <是否限时（1是0否）> <保持时长(小时 限时为1时需>0)>//仅限骰主使用";
+cmdAddItemInfo.help = ".添加道具信息 <名称> <影响变量（例：$m金币，若为不可使用道具，填写#）> <影响变量参数（=+-*操作符开头，例：+1即为金币+1，在提前定义了最大值的情况下支持%表达）> <描述> <使用回执> <是否限时（1是0否）> <保持时长(小时 限时为1时需>0)>//仅限骰主使用";
 cmdAddItemInfo.solve = (ctx, msg, args) => {
     if (args.getArgN(1) === "help") {
         let ret = seal.ext.newCmdExecuteResult(true);
@@ -261,7 +283,7 @@ cmdAddItemInfo.solve = (ctx, msg, args) => {
 
     console.log(`~ ${name}\n描述: ${discreption}\n使用回执: ${useMsg}\n影响变量: ${effectType}:${effect}\n是否限时状态：${ifStatus}保持时长：${upTime}`);
     if (!name || isNaN(ifStatus) || isNaN(upTime)) {
-        seal.replyToSender(ctx, msg, "参数错误。参考：.添加道具信息 <名称> <影响变量> <影响变量参数（=+-*操作符开头）> <描述> <使用回执> <是否限时（1是0否）> <保持时长(小时 限时为1时需>0)\n最后两个参数不填默认为非状态变更道具");
+        seal.replyToSender(ctx, msg, "参数错误。参考：.添加道具信息 <名称> <影响变量（例：$m金币，若为不可使用道具，填写#）> <影响变量参数（=+-*操作符开头）> <描述> <使用回执> <是否限时（1是0否）> <保持时长(小时 限时为1时需>0)\n最后两个参数不填默认为非状态变更道具");
         return seal.ext.newCmdExecuteResult(true);
     }
     if(ifStatus==1&&upTime<=0){
@@ -306,7 +328,7 @@ ext.cmdMap["删除道具信息"] = cmddelItemInfo;
 
 let cmdlistItemInfo = seal.ext.newCmdItemInfo();
 cmdlistItemInfo.name = "listItemInfo";
-cmdlistItemInfo.help = ".展示道具 //展示道具信息，仅限骰主使用 TODO:查询道具信息（骰主版） 按页数查询";
+cmdlistItemInfo.help = ".展示道具 <页数（为空默认为1）> //仅限骰主使用 按页数查询";
 cmdlistItemInfo.solve = (ctx, msg, args) => {
     if (args.getArgN(1) === "help") {
         let ret = seal.ext.newCmdExecuteResult(true);
@@ -317,10 +339,15 @@ cmdlistItemInfo.solve = (ctx, msg, args) => {
         seal.replyToSender(ctx, msg, seal.formatTmpl(ctx, "无权限"));
         return seal.ext.newCmdExecuteResult(true);
     }
-    let page = args.getArgN(1)||1;
+    let page = parseInt(args.getArgN(1))||1;
+
+    if (page<=0) {
+        seal.replyToSender(ctx, msg, "页数应>0");
+        return seal.ext.newCmdExecuteResult(true);
+    }
 
     let itemInfo = new ItemInfo();
-    let items = itemInfo.present();
+    let items = itemInfo.present(page);
 
     seal.replyToSender(ctx, msg, `道具信息<`+page+`>：\n${items}\n查看道具全部信息请.检索道具信息 <名称>`);
 
@@ -405,14 +432,16 @@ cmduseItem.solve = (ctx, msg, args) => {
         return ret;
     }
     let name = args.getArgN(1);
-    let quantity = parseInt(args.getArgN(2))||1;
+    let quantity = 1;
+    if(!isNaN(args.getArgN(2))){
+        quantity = parseInt(args.getArgN(2));
+    }
 
-    //const mctx = getCtxProxyFirst(ctx, msg)||ctx;//不好用 获取不到 有空自己写一个反解cq码呜呜呜
-    const mctx = ctx;
+    const mctx = seal.getCtxProxyFirst(ctx, args)||ctx;
     console.log(`道具生效于：`+mctx.player.userId);
 
-    if (!name || isNaN(quantity) || quantity<=0) {
-        seal.replyToSender(ctx, msg, "参数错误。用法：.使用 <名称> <数量(大于0)>");
+    if (!name || quantity<=0) {
+        seal.replyToSender(ctx, msg, "参数错误。用法：.使用 <名称> <数量(大于0)> <目标（at） 为空则指自己>");
         return seal.ext.newCmdExecuteResult(true);
     }
 
@@ -424,16 +453,13 @@ cmduseItem.solve = (ctx, msg, args) => {
         seal.replyToSender(ctx, msg, `使用失败：${UseErrno[retNum]}`);
     }
     else {
-        if(mctx.player.userId!==ctx.player.userId){
-            seal.replyPerson(mctx, msg, `${ctx.player.name}对你使用了${quantity}个${name}!`);
-        }
         seal.replyToSender(ctx, msg, itemOverview.useMsg);
     }
     return seal.ext.newCmdExecuteResult(true);
 };
 ext.cmdMap["使用"] = cmduseItem;
 
-//=========================================================================测试期间特供指令（或者说是内部？）
+//=======================================================================================查错用指令 不出问题的情况下不推荐使用
 let cmdcheckValue = seal.ext.newCmdItemInfo();
 cmdcheckValue.name = "checkValue";
 cmdcheckValue.help = ".查看属性 <名称（例：$mHP）> <目标（at,为空则默认为自己）>//查看属性";
@@ -445,8 +471,8 @@ cmdcheckValue.solve = (ctx, msg, args) => {
         return ret;
     }
     let name = args.getArgN(1);
-    const mctx = seal.getCtxProxyFirst(ctx, msg)||ctx;
-    if (!name) {
+    const mctx = seal.getCtxProxyFirst(ctx, args)||ctx;
+        if (!name) {
         seal.replyToSender(ctx, msg, "参数错误。用法：.查看属性 <名称> <目标（at,为空则默认为自己）>");
         return seal.ext.newCmdExecuteResult(true);
     }
@@ -458,3 +484,36 @@ cmdcheckValue.solve = (ctx, msg, args) => {
     return seal.ext.newCmdExecuteResult(true);
 };
 ext.cmdMap["查看属性"] = cmdcheckValue;
+
+let cmdgetItem = seal.ext.newCmdItemInfo();
+cmdgetItem.name = "getItem";
+cmdgetItem.help = ".强制获取 <名称> <数量>  <目标（at） 为空则指自己>//仅限骰主使用";
+cmdgetItem.allowDelegate = true;
+cmdgetItem.solve = (ctx, msg, args) => {
+    if (args.getArgN(1) === "help") {
+        let ret = seal.ext.newCmdExecuteResult(true);
+        ret.showHelp = true;
+        return ret;
+    }
+    let name = args.getArgN(1);
+    let quantity = 1;
+    if(!isNaN(args.getArgN(2))){
+        quantity = parseInt(args.getArgN(2));
+    }
+
+    const mctx = seal.getCtxProxyFirst(ctx, args)||ctx;
+    if (ctx.privilegeLevel < 100) {
+        seal.replyToSender(ctx, msg, seal.formatTmpl(ctx, "无权限"));
+        return seal.ext.newCmdExecuteResult(true);
+    }
+    if (!name || quantity<=0) {
+        seal.replyToSender(ctx, msg, "参数错误。用法：.强制获取 <名称> <数量> <目标（at） 为空则指自己>");
+        return seal.ext.newCmdExecuteResult(true);
+    }
+
+    let backpack = new Rucksack(mctx);
+    backpack.placeItem({name, quantity});
+    seal.replyToSender(ctx, msg, `强制向${mctx.player.name}的背包里塞了${quantity}个${name}`);
+    return seal.ext.newCmdExecuteResult(true);
+};
+ext.cmdMap["强制获取"] = cmdgetItem;
